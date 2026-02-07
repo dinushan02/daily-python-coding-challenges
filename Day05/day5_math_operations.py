bf7319915f546fb03cef2937d5966f693d673694
Dinushan <dinushandinumoorthy1111@gmail.com>
2026-02-07 20:36:21 +0530

    Added Day 5: Math Operations challenge

---
+++ Day05/day5_math_operations.py
@@ -0,0 +1,24 @@
+# Day 5: Math Operations
+
+print("*==============================*")
+print("|         CALCULATOR 📟        |")
+print("*==============================*\n")
+
+num1 = float(input("➡️ Enter first number: "))
+num2 = float(input("➡️ Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\n*==============================*")
+print("|          RESULTS ✅          |")
+print("*==============================*\n")
+
+print(f"➕ Addition: {addition}")
+print(f"➖ Subtraction: {subtraction}")
+print(f"✖️ Multiplication: {multiplication}")
+print(f"➗ Division: {division:.2f}")  # Here, 2f is used for 2 decimal points
+
+print("\n✨ Thanks for calculating! ✨")
